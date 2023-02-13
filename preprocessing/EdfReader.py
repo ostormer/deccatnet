db2353bf5d0d4d312a9b445a00852e3d1bdc1e25
Oskar Størmer <oskar.stormer@gmail.com>
2023-02-13 16:43:50 +0100

    mne testing, learning channel manipulation etc

--- preprocessing/EdfReader.py
+++ preprocessing/EdfReader.py
@@ -1,10 +1,13 @@
-import mne
+from mne.io import read_raw_edf
 
 class EDF_reader:
 
     def __init__(self, filename):
-        self.edf_file = mne.io.read_raw_edf(filename)
-
+        self.edf_file = read_raw_edf(filename)
+        # TODO: MNE handles channels with different sample rates poorly,
+        # blockwise resampling to the highest sample rate
+        # Maybe handle this by detecting and dropping channels
+        # with weird sample rates
 
     def raw_edf(self):
         return self.edf_file.get_data()
