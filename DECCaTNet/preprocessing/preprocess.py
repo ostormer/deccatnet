6492b386614241e122e7779113775ea3f9d2156b
Styrkhk <styrkhk@stud.ntnu.no>
2023-05-09 10:28:09 +0200

    debugging for error this morgning

--- DECCaTNet/preprocessing/preprocess.py
+++ DECCaTNet/preprocessing/preprocess.py
@@ -636,7 +636,10 @@ def _preproc_split(ds_params, global_params, dataset=None):
     split_save_dir = ds_params['split_save_dir']
     cache_dir = ds_params['cache_dir']
 
+
     batch_dirs = sorted(os.listdir(preproc_save_dir))
+
+    print(f'Debugging, bathc dirs is: {batch_dirs}')
     # Calculate preprocessed ds length, which is max stop_idx value
     preproc_ds_length = (len(batch_dirs) - 1) * 500 + len(os.listdir(os.path.join(preproc_save_dir, batch_dirs[-1])))
     if stop_idx is None or stop_idx > preproc_ds_length:
