cbb5ada1f720c52ddff2e640362036f135ad717f
Styrkhk <styrkhk@stud.ntnu.no>
2023-05-10 09:56:05 +0200

    keep testing

--- DECCaTNet/DECCaTNet_model/hyper_search.py
+++ DECCaTNet/DECCaTNet_model/hyper_search.py
@@ -205,7 +205,7 @@ def fine_tuning_hypersearch(all_params=None, global_params=None, test_set=None):
 
     all_params_2 = copy.deepcopy(all_params)
     all_params_2['fine_tuning']['encoder_path'] = os.path.join(os.getcwd(), all_params_2['fine_tuning']['encoder_path'])
-    print(all_params_2['fine_tuning']['encoder_path'])
+    print(all_params_2['fine_tuning']['encoder_path'], f"this is what it should be {os.path.join(os.getcwd(), all_params_2['fine_tuning']['encoder_path'])}")
     model = FineTuneNet(channel_groups, ds_channel_order, all_params_2, global_params)
     #model = FineTuneNet(channel_groups, ds_channel_order, all_params, global_params) # TODO remove this change
     try:
