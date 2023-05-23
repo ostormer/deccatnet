76b0497daba3dfb3b352159f6301326bb2abbce0
Styrkhk <styrkhk@stud.ntnu.no>
2023-05-23 12:40:50 +0200

    added some printing

--- DECCaTNet/DECCaTNet_model/fine_tuning.py
+++ DECCaTNet/DECCaTNet_model/fine_tuning.py
@@ -136,7 +136,8 @@ class FineTuneNet(nn.Module):
 
 
 def n_correct_preds(y_pred, y):
-    #print(y_pred,y)
+    print(y_pred,y)
+    print(torch.argmax(y_pred,dim=1))
     num_correct = (torch.argmax(y_pred, dim=1) == y).float().sum().item()
     num_total = len(y)
     #print(f'checking that n_correct_preds work: {y_pred} and y: {y}, gives num correct {num_correct}')
