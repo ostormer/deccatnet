c7a65182f24320e670502ba7ceba4015b3dbc678
Styrkhk <styrkhk@stud.ntnu.no>
2023-05-23 15:22:09 +0200

    look for exploding and etc gradients.

--- DECCaTNet/DECCaTNet_model/fine_tuning.py
+++ DECCaTNet/DECCaTNet_model/fine_tuning.py
@@ -214,6 +214,7 @@ def train_epoch(model, train_loader, device, loss_func, optimizer,disable):
         loss = loss_func(pred, y)
         # update weights
         loss.backward()
+        plot_grad_flow(model.named_parameters())
         optimizer.step()
 
         correct, number = n_correct_preds(pred, y)
@@ -393,8 +394,8 @@ def run_fine_tuning(all_params, global_params, test_set=None):
     # im thinking load one window
     ds_channel_order = dataset.__getitem__(0, window_order=True)
 
-    for i in range(math.floor(len(idx) * all_params['hyper_search']['fine_tune_split'])):
-        window_order = dataset.__getitem__(i, window_order=True)
+    #for i in range(math.floor(len(idx) * all_params['hyper_search']['fine_tune_split'])):
+     #   window_order = dataset.__getitem__(i, window_order=True)
         # if not window_order == ds_channel_order:
         #     changes = [ds_channel_order.index(ch_n) if ds_channel_order[i] != ch_n else i for i, ch_n in
         #                enumerate(window_order)]
@@ -505,3 +506,32 @@ def run_fine_tuning(all_params, global_params, test_set=None):
 def get_window_len(ds):
     diff = ds.windows.metadata['i_stop_in_trial'] - ds.windows.metadata['i_start_in_trial']
     return diff.to_numpy()
+
+
+def plot_grad_flow(named_parameters):
+    '''Plots the gradients flowing through different layers in the net during training.
+    Can be used for checking for possible gradient vanishing / exploding problems.
+
+    Usage: Plug this function in Trainer class after loss.backwards() as
+    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow'''
+    ave_grads = []
+    max_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if (p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+            max_grads.append(p.grad.abs().max())
+    plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+    plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+    plt.hlines(0, 0, len(ave_grads) + 1, lw=2, color="k")
+    plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(left=0, right=len(ave_grads))
+    plt.ylim(bottom=-0.001, top=0.02)  # zoom in on the lower gradient regions
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.legend([Line2D([0], [0], color="c", lw=4),
+                Line2D([0], [0], color="b", lw=4),
+                Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])
