9400ce7b28412bb19efc769f627d96b7c1190cbc
Styrkhk <styrkhk@stud.ntnu.no>
2023-05-24 11:53:48 +0200

    change batch size

--- DECCaTNet/DECCaTNet_model/fine_tuning.py
+++ DECCaTNet/DECCaTNet_model/fine_tuning.py
@@ -221,7 +221,7 @@ def train_epoch(model, train_loader, device, loss_func, optimizer,disable):
         # update weights
         loss.backward()
 
-        if count % 20 == 0:
+        if count % 40 == 0:
             print(f'the obtained loss is {loss.item()} and we have the followinf predictions: {pred} for the followinf targets: {y}')
             print(f'resulting in prediction accuracy: {n_correct_preds(pred,y)}')
         #     plot_grad_flow(model.cpu().named_parameters(),count)
